7a8a8c83e1fd0cfd8c5debd0f07efa064b9f60d1
Olivier Pons <olivier.pons@gmail.com>
2024-05-26 13:46:05 +0200

    Update levels 01-05

--- export/index.service.worker.js
+++ export/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1716638236|211740777";
+const CACHE_VERSION = "1716648468|4901340064";
 const CACHE_PREFIX = "ProutForm-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
