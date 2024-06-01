df682a338c74a3c2695a0e2ee367f05846511c99
Olivier Pons <olivier.pons@gmail.com>
2024-06-02 00:11:26 +0200

    Smaller exe

--- export/index.service.worker.js
+++ export/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1717273185|7616178039";
+const CACHE_VERSION = "1717279871|12255182";
 const CACHE_PREFIX = "ProutForm-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
