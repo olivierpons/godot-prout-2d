21b9db405d3aed509bf589b67d19f1e4e1e1a685
Olivier Pons <olivier.pons@jiliti-group.com>
2024-06-07 18:20:36 +0200

    New joystick should work this time!

--- scripts/mobile/joystick.gd
+++ scripts/mobile/joystick.gd
@@ -3,53 +3,43 @@ extends Node2D
 @onready var knob = $knob
 @onready var label = $Label
 @export var return_duration: float =  0.5
-@export var circle_size: float = 100  # Ajustez cette valeur selon vos besoins
+@export var circle_size: float = 100
 var current_direction: Vector2 = Vector2.ZERO
 var index_dragging: int = -1
 
-var knob_original_position: Vector2
+var knob_local_position: Vector2
 
 func _ready():
-	knob_original_position = knob.global_position
+	knob_local_position = knob.position
 
 func _input(event):
 	if event is InputEventScreenDrag and index_dragging >= 0:
+		var local_mouse_pos: Vector2 = to_local(get_global_mouse_position())
 		if event.get_index() == index_dragging:
-			print("InputEventScreenDrag: ", event.get_index())
-			update_knob_position(get_global_mouse_position())
+			update_knob_position(local_mouse_pos)
 	elif event is InputEventScreenTouch:
+		var local_mouse_pos: Vector2 = to_local(get_global_mouse_position())
 		if event.pressed:
-			print("InputEventScreenTouch pressed ", event.position, ": ", event.get_index())
-			if is_inside_circle(get_global_mouse_position()):
-				print("is_inside_circle()!")
+			if (local_mouse_pos - knob.position).length() <= circle_size:
 				index_dragging = event.get_index()
-				update_knob_position(get_global_mouse_position())
+				update_knob_position(local_mouse_pos)
 		else:
-			print("InputEventScreenTouch released: ", event.get_index())
 			index_dragging = -1
-			_on_joystick_released()
-
-func _on_joystick_pressed():
-	print("_on_joystick_pressed")
-#	if is_inside_circle(get_global_mouse_position()):
-#		is_dragging = true
-#		update_knob_position(get_global_mouse_position())
-
-func _on_joystick_released():
-	if index_dragging >= 0:
-		return
-	print("_on_joystick_released")
-	var tween = create_tween()
-	tween.tween_property(
-		knob, "position", knob_original_position, return_duration
-	).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-	reset_input_actions()
+			var tween = create_tween()
+			tween.tween_property(
+				knob, "position", knob_local_position, return_duration
+			).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+			# reset_input_actions:
+			Input.action_release("move_left")
+			Input.action_release("move_right")
+			Input.action_release("move_up")
+			Input.action_release("move_down")
 
 func update_knob_position(new_position: Vector2):
-	var offset = to_local(new_position) - knob_original_position
+	var offset = new_position - knob_local_position
 	if offset.length() > circle_size:
 		offset = offset.normalized() * circle_size
-	knob.position = knob_original_position + offset
+	knob.position = knob_local_position + offset
 	var joypad_value = offset / circle_size
 	label.text = "Joypad Value: " + str(joypad_value)
 
@@ -96,14 +86,3 @@ func update_input_actions(direction: Vector2):
 		Input.action_release("move_up")
 		Input.action_release("move_down")
 
-func reset_input_actions():
-	Input.action_release("move_left")
-	Input.action_release("move_right")
-	Input.action_release("move_up")
-	Input.action_release("move_down")
-
-func is_inside_circle(position: Vector2) -> bool:
-	print("position: ", position, ", knob.global_position: ", knob.global_position)
-	var diff = position - knob.global_position
-	#print("diff.length(): ", diff.length())
-	return diff.length() <= circle_size
